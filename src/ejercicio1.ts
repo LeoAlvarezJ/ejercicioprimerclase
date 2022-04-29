52aaf64b6807d88b7370f534b11cf5d7a6938d33
CintiaAlvarez <104107300+CintiaAlvarez@users.noreply.github.com>
2022-04-29 06:22:51 -0300

    Initial commit

---
+++ src/ejercicio1.ts
@@ -0,0 +1,6 @@
+console.log("crear un posteo en facebook");
+
+console.log("paso 1");
+console.log("paso 2");
+console.log("paso 3");
+console.log("paso 4");
---
+++ src/ejercicio2.ts
@@ -0,0 +1,13 @@
+console.log("la granjera");
+
+console.log("mover hacia adelante y cavar");
+console.log("mover hacia adelante y cavar");
+console.log("mover hacia adelante y cavar");
+console.log("mover hacia adelante y cavar");
+console.log("girar hacia la izq");
+console.log("mover hacia adelante y cavar");
+console.log("mover hacia adelante y cavar");
+console.log("mover hacia adelante y cavar");
+console.log("mover hacia adelante y cavar");
+console.log("mover hacia adelante y cavar");
+console.log("mover hacia adelante y cavar");
